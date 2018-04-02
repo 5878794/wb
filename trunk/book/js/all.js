3e4b5c9690bedb7d32cae6d1b691450ed3bcf3b8
beens <5878794@qq.com>
2018-04-02 22:31:11 +0800

    mdf top show

---
+++ trunk/book/js/all.js
@@ -0,0 +1,62 @@
+var DEVICE = {};
+(function () {
+	var Sys = {};
+	var ua = navigator.userAgent.toLowerCase();
+	var s;
+	(s = ua.match(/ipad; cpu os ([\d_]+)/)) ? Sys.ipad = s[1].replace(/_/g, ".") :
+		(s = ua.match(/iphone os ([\d_]+)/)) ? Sys.iphone = s[1].replace(/_/g, ".") :
+			(s = ua.match(/android[ \/]([\d.]+)/)) ? Sys.android = s[1] :
+				(s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1] :
+					(s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+						(s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+							(s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+								(s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+									(s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : Sys._ = 0;
+
+
+	DEVICE.isIpad = (Sys.hasOwnProperty("ipad"));
+	DEVICE.isIphone = (Sys.hasOwnProperty("iphone"));
+	DEVICE.isAndroid = (Sys.hasOwnProperty("android"));
+	DEVICE.isIe = (Sys.hasOwnProperty("ie"));
+	DEVICE.isFirefox = (Sys.hasOwnProperty("firefox"));
+	DEVICE.isChrome = (Sys.hasOwnProperty("chrome"));
+	DEVICE.isOpera = (Sys.hasOwnProperty("opera"));
+	DEVICE.isSafari = (Sys.hasOwnProperty("safari"));
+	DEVICE.isWeChat = (ua.match(/MicroMessenger/i) == "micromessenger");
+
+	DEVICE.ver = 0;
+	var ver;
+	for (var key in Sys) {
+		if (Sys.hasOwnProperty(key)) {
+			ver = Sys[key];
+		}
+	}
+	ver = ver.split(".");
+	var _ver = [];
+	for (var i = 0, l = ver.length; i < l; i++) {
+		if (i >= 2) {
+			break;
+		}
+		_ver.push(ver[i]);
+	}
+	_ver = _ver.join(".");
+	DEVICE.ver = _ver;
+
+	DEVICE.isPhone = (DEVICE.isAndroid || DEVICE.isIpad || DEVICE.isIphone);
+})();
+
+
+$(document).ready(function(){
+	//处理头部显示
+	handlerTopInfo();
+
+});
+
+
+var handlerTopInfo = function(){
+	if(DEVICE.isIpad || DEVICE.isIphone){
+		$('#top_dom').find('.top_pos').css({display:'block'});
+	}else{
+		$('#top_dom').find('.top_back').css({display:'block'});
+	}
+};
